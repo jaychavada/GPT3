81733aec84c48deb322d87d8b192eb02695fabef
Jay chavada <chavdajay243@gmail.com>
2024-02-23 09:28:49 +0530

    navbar updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,28 +6,30 @@ import "./navbar.css";
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
 
+  const links = [
+    { id: "#home", text: "Home" },
+    { id: "#wgpt3", text: "What is GPT3?" },
+    { id: "#possibility", text: "Open AI" },
+    { id: "#features", text: "Case Studies" },
+    { id: "#blog", text: "Library" },
+  ];
+
+  const toggleMenuHandler = () => {
+    setToggleMenu((prev) => !prev);
+  };
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo} alt="Logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p>
-            <a href="#home">Home</a>
-          </p>
-          <p>
-            <a href="#wgpt3">What is GPT3?</a>
-          </p>
-          <p>
-            <a href="#possibility">Open AI</a>
-          </p>
-          <p>
-            <a href="#features">Case Studies</a>
-          </p>
-          <p>
-            <a href="#blog">Library</a>
-          </p>
+          {links.map((link) => (
+            <p key={link.id}>
+              <a href={link.id}>{link.text}</a>
+            </p>
+          ))}
         </div>
       </div>
       <div className="gpt3__navbar-sign">
@@ -36,36 +38,18 @@ const Navbar = () => {
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu ? (
-          <RiCloseLine
-            color="#fff"
-            size={27}
-            onClick={() => setToggleMenu(false)}
-          />
+          <RiCloseLine color="#fff" size={27} onClick={toggleMenuHandler} />
         ) : (
-          <RiMenu3Line
-            color="#fff"
-            size={27}
-            onClick={() => setToggleMenu(true)}
-          />
+          <RiMenu3Line color="#fff" size={27} onClick={toggleMenuHandler} />
         )}
         {toggleMenu && (
           <div className="gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
-              <p>
-                <a href="#home">Home</a>
-              </p>
-              <p>
-                <a href="#wgpt3">What is GPT3?</a>
-              </p>
-              <p>
-                <a href="#possibility">Open AI</a>
-              </p>
-              <p>
-                <a href="#features">Case Studies</a>
-              </p>
-              <p>
-                <a href="#blog">Library</a>
-              </p>
+              {links.map((link) => (
+                <p key={link.id}>
+                  <a href={link.id}>{link.text}</a>
+                </p>
+              ))}
             </div>
             <div className="gpt3__navbar-menu_container-links-sign">
               <p>Sign in</p>
